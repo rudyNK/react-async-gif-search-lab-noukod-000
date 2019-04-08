06ec9b949058735fa5b84efbd9cc748f667c7b87
Rudy Ravelin <rudy@noukod.com>
2019-04-08 15:46:58 +0000

    Automatically backed up by Learn

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -1,26 +1,11 @@
 import React from 'react';
-import GifSearch from '../components/GifSearch';
-import GifList from '../components/GifList';
-export default class GifListContainer extends React.Component {
-  state = {
-    gifs: []
-  }
-  fetchGIFs = (query) => {
-    fetch(`http://api.giphy.com/v1/gifs/search?q=${query}&api_key=dc6zaTOxFJmzC&rating=g`)
-    .then(response => response.json())
-    .then(data => {
-      this.setState({
-        gifs: data.data
-      })
-      console.log(this.state.gifs)
-    })
-  }
+export default class GifList extends React.Component {
+
   render() {
     return(
-      <div>
-        <GifSearch getGIFs={this.fetchGIFs} />
-        <GifList gifs={this.state.gifs} />
-      </div>
+      <ul>
+        {this.props.gifs.map(gif => <li key={gif.id}><img src={gif.images.original.url}></img></li>)}
+      </ul>
     )
   }
 }
